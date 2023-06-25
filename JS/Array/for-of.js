11541c81dc831eacb04194cf2bb27ba06e5b0b66
Tiago Souza <tiago0214@gmail.com>
2023-06-24 23:00:40 -0300

    att

---
+++ JS/Array/for-of.js
@@ -0,0 +1,8 @@
+const notas = [10,6.5,8,7.5]
+
+for(let nota of notas){ //percorre os elementos do meu array
+    console.log(nota)
+}
+for(let i=0;i<notas.length;i++){
+    console.log(notas[i])
+}
---
+++ JS/Array/test.js
@@ -0,0 +1,19 @@
+// const test = [100,200,300,400,500,600]
+
+// // for(i = test.length -1 ; i >= 0; i --){
+// //     console.log(test[i])
+// // }
+
+// for(i = test.length -1; test[i] >= 300 ; i -- ){
+//     console.log(test[i])
+// }
+
+
+//for é uma estrutura de repetição que eu posso usar para tudo! loops(laço de repetição)
+const numerosPares = []
+
+for(i = 0; i<=100 ; i +=2){
+    numerosPares.push(i)
+}
+
+console.log(numerosPares)
