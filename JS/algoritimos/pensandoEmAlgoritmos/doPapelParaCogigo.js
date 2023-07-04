e1bfa93a03808ac5a02653deafd9669be4b585dc
Tiago Souza <tiago0214@gmail.com>
2023-07-03 22:32:40 -0300

    att

--- JS/algoritimos/pensandoEmAlgoritmos/doPapelParaCogigo.js
+++ JS/algoritimos/pensandoEmAlgoritmos/doPapelParaCogigo.js
@@ -1,10 +1,10 @@
-const precos = [25, 15, 30, 50, 45, 20]
+const livros = require("./listaLivros")
 
 let maisBarato = 0;
 
-for (let atual = 0; atual < precos.length ; atual++){
-    if(precos[atual] < precos[maisBarato]){
-        maisBarato= precos[atual]
+for (let atual = 0; atual < livros.length ; atual++){
+    if(livros[atual].valor < livros[maisBarato].valor){
+        maisBarato= livros[atual]
     }
 }
 //ele vai passar por todo o meu array primeiro, para depois ir para o console.
---
+++ JS/algoritimos/pensandoEmAlgoritmos/listaLivros.js
@@ -0,0 +1,8 @@
+const precos = [
+    {titulo: 'JS',
+    valor: 25},
+    {titulo:'py',
+    valor: 15}, 
+    ]
+
+module.exports = precos;
