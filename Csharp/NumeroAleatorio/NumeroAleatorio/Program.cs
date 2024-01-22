a069ab9a7bfdd7999531983a297a3acef5ee4adb
Tiago Souza <tiago0214@gmail.com>
2024-01-21 21:06:24 -0300

    att

---
+++ Csharp/NumeroAleatorio/NumeroAleatorio/Program.cs
@@ -0,0 +1,26 @@
+﻿Random num = new Random();
+int aleatorio = num.Next(1, 101);
+
+Console.Write("Digite um número 1 - 100: ");
+
+
+do
+{
+    int chute = int.Parse(Console.ReadLine());
+    if (chute == aleatorio)
+    {
+        Console.WriteLine("Você acertou o número");
+        break;
+    }
+    else if (chute < aleatorio)
+    {
+        Console.WriteLine("O numero é maior");
+        Console.WriteLine("Digite novamente");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor.");
+        Console.WriteLine("Digite novamente");
+    }
+
+} while (true);
