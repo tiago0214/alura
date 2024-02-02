527559fc859c0fe33d971258791abb3d71ab0761
Tiago Souza <tiago0214@gmail.com>
2024-02-02 18:43:01 -0300

    att

---
+++ Csharp/ScreenSound-04/ScreenSound-04/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+    //vou criar uma lista baseada no objeto ai de cima em JSON.
+    //reparar que eu não estou instanciando o TIPO . JsonSerializer.
+    var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+}
+
